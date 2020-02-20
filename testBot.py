5cf74b29abd6e70cd966305d4b7aaaaab2d7f983
Sean Conn <sconn823@gmail.com>
2020-02-19 22:54:17 -0500

    Implemented random choice and filled lists

--- testBot.py
+++ testBot.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import random
 
 def remove_slash_ns(input_list):
     return [word.rstrip('\n') for word in input_list]
@@ -20,7 +21,12 @@ def createLists(peopleListLocation, templateListLocation):
     template_list = remove_slash_ns(template_list)
     return people_list, template_list
 
+def find_and_replace(person, template):
+    '''As of 2/19/20, there is only variable per template. This may change in the future, so I have created a method in order to make it easier to change in the future.'''
+    return template.replace("PERSON", person)
+
 if __name__ == "__main__":
-    print(
+    people_list, template_list = (
         createLists("people.txt", "template.txt")
         )
+    print(find_and_replace(random.choice(people_list), random.choice(template_list)))
